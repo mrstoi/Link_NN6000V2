e335a922595cf4e658d2c78f1127d585aec4b475
wzdddyy <89689685+wzdddyy@users.noreply.github.com>
2026-03-19 08:17:06 +0800

    Update packages.sh

--- wrt_core/modules/packages.sh
+++ wrt_core/modules/packages.sh
@@ -19,7 +19,7 @@ remove_unwanted_packages() {
     )
     local small8_packages=(
         "ppp" "firewall" "dae" "daed" "daed-next" "libnftnl" "nftables" "dnsmasq" "luci-app-alist"
-        "alist" "opkg" "smartdns" "luci-app-smartdns" "easytier"
+        "alist" "opkg" "smartdns" "luci-app-smartdns" "easytier" "webd" "luci-app-natmap"
     )
 
     for pkg in "${luci_packages[@]}"; do
